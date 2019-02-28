ee60163a8ffd0a0d722bfa0b8963b8b6391953a4
colebryant <cobryant47@gmail.com>
2019-02-28 14:32:07 -0600

    implementing heroku code

--- src/Callback.js
+++ src/Callback.js
@@ -6,9 +6,6 @@ import MixMateIcon from '../src/components/images/cocktail-shaker.png'
 class Callback extends Component {
   async componentDidMount() {
     await auth0Client.handleAuthentication();
-    // The getProfile method allows you to get information about the logged in user
-    // Use the "sub" property to tie the user in your database to the user in Auth0. This
-    // Means you will add a new property called "auth0Identifier" to your user object.
     this.props.history.replace('/profile');
   }
 
